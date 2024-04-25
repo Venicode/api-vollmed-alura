8769cc334c616a787ea9ce6e9d20a9d6a7d404a3
venicode <vitoria_aparecida2011@hotmail.com>
2024-04-25 16:15:16 -0300

    finalizando o CRUD da api

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,15 +1,13 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.DadosListagemMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -22,7 +20,29 @@ public class MedicoController {
         repository.save(new Medico(dados));
     }
     @GetMapping
-    public List<DadosListagemMedico> listar(){
-        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    public Page<DadosListagemMedico> listarPorPaginaAtivos(@PageableDefault(size=10, sort = {"nome"}) Pageable paginacao){
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+    }
+    @PutMapping
+    @Transactional //O transactional serve para escrever e sobrescrever no banco de dados
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+        var medico = repository.getReferenceById(dados.id());
+        medico.atualizarInformacoes(dados);
+    }
+    /*
+    Exclusão diretamente no banco de dados
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id){
+       repository.deleteById(id);
+    }
+    */
+
+    //Exclusão lógica
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void desativar(@PathVariable Long id){
+        var medico = repository.getReferenceById(id);
+        medico.desativar();
     }
 }
